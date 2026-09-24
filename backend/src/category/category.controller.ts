3b02733a5342941060e3396cb4ec0b54fd04cbed
Minh Hang <whales.iluca@gmail.com>
2026-09-24 01:51:23 +0000

    Implement CategoryService CRUD

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { CategoryService } from './category.service';
+import { Category } from './category.entity';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  findAll(): Promise<Category[]> {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Category | null> {
+    return this.categoryService.findOne(Number(id));
+  }
+
+  @Post()
+  create(@Body() data: Partial<Category>): Promise<Category> {
+    return this.categoryService.create(data);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() data: Partial<Category>,
+  ): Promise<Category | null> {
+    return this.categoryService.update(Number(id), data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.categoryService.remove(Number(id));
+  }
+}
--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Category } from './category.entity';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @Inject('CATEGORY_REPOSITORY')
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  // Hiển thị tất cả category
+  async findAll(): Promise<Category[]> {
+    return this.categoryRepository.find();
+  }
+
+  // Hiển thị một category theo ID
+  async findOne(category_id: number): Promise<Category | null> {
+    return this.categoryRepository.findOne({
+      where: { category_id },
+    });
+  }
+
+  // Thêm category
+  async create(data: Partial<Category>): Promise<Category> {
+    const category = this.categoryRepository.create(data);
+    return this.categoryRepository.save(category);
+  }
+
+  // Cập nhật category
+  async update(
+    category_id: number,
+    data: Partial<Category>,
+  ): Promise<Category | null> {
+    await this.categoryRepository.update(category_id, data);
+
+    return this.categoryRepository.findOne({
+      where: { category_id },
+    });
+  }
+
+  // Xóa category
+  async remove(category_id: number): Promise<void> {
+    await this.categoryRepository.delete(category_id);
+  }
+}
